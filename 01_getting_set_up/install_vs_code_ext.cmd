c6ea808deb51af6ede66765c9d42a63760dcba8e
Cory Taylor <coryandrewtaylor@gmail.com>
2020-04-17 11:38:10 -0500

    Create install_vs_code_ext.cmd
    install_vs_code_ext.cmd installs several VS Code extensions from the command line.

---
+++ 01_getting_set_up/install_vs_code_ext.cmd
@@ -0,0 +1,5 @@
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension ms-python.python
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension njpwerner.autodocstring
+code --install-extension redhat.vscode-xml
