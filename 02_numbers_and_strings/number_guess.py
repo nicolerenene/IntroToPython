8d0f3a52aade47ae0b11d87ef0e8217c45403560
Cory Taylor <coryandrewtaylor@gmail.com>
2020-04-24 16:52:01 -0500

    Add instructions to number_guess.py

--- 02_numbers_and_strings/number_guess.py
+++ 02_numbers_and_strings/number_guess.py
@@ -1,1 +1,6 @@
-# Your code here...
+# Code in a hidden number as a variable
+# Prompt user to enter a number
+# Sanitize user input
+# Convert user input to numeric type (float is best)
+# Find out whether user number >, <, == hidden number
+# Print the result of equality check
