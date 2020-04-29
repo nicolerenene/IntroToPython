516fdd36d30bdcdf5e068735d5c730c36c1b2d3a
nicolerenene <64162382+nicolerenene@users.noreply.github.com>
2020-04-29 13:44:22 -0500

    ok

--- 02_numbers_and_strings/number_guess.py
+++ 02_numbers_and_strings/number_guess.py
@@ -4,3 +4,13 @@
 # Convert user input to numeric type (float is best)
 # Find out whether user number >, <, == hidden number
 # Print the result of equality check
+
+hidden_number = 222
+user_number = input('Try to guess the hidden number: ')
+user_number = float(user_number.strip())
+if user_number > hidden_number:
+    print(str(user_number) + ' is greater than the hidden number')
+elif user_number < hidden_number:
+    print(str(user_number) + ' is less than the hidden number')
+else:
+    print(str(user_number) + ' is the hidden number!')
