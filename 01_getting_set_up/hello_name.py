cd03aa3ea9197b5ef408c2ad3885f51742eca25f
nicolerenene <64162382+nicolerenene@users.noreply.github.com>
2020-04-29 13:44:13 -0500

    ok

--- 01_getting_set_up/hello_name.py
+++ 01_getting_set_up/hello_name.py
@@ -1,5 +1,5 @@
-user_name = input('Please type your name and hit Enter: ')
-print('Hello,', user_name + '!')
+#user_name = input('Please type your name and hit Enter: ')
+#print('Hello,', user_name + '!')
 
 # New output:
 # Hello CORY, your name is 4 letters long.
@@ -8,3 +8,7 @@ print('Hello,', user_name + '!')
 #   * ''.strip()
 #   * ''.upper()
 #   * len('')
+user_name = input('Type your name: ')
+user_name = user_name.strip()
+name_count = len(user_name)
+print('Hello, ' + user_name.upper() + ' your name is ' + str(name_count) + ' letters long')
